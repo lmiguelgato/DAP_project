f2b55f428e93c1ace55b48c36765783d239519f1
Luis M. Gato <lmiguelgato@gmail.com>
2019-10-21 19:50:09 -0500

    Add comments

--- matlab/kalmanFilter.m
+++ matlab/kalmanFilter.m
@@ -1,32 +1,42 @@
 function [ x, P ] = kalmanFilter( x_, y, P, acc, tau )
 % Kalman filter
-% input: x_ (previous state)
-% output: x (current state)
+
+% input:    x_  (previous state)
+%           y   (current measurement)
+%           P   (previous-state error posterior covariance matrix)
+%           acc (acceleration from the movement model)
+%           tau (observation period)
+
+% output:   x   (current state)
+%           P   (current-state error posterior covariance matrix)
+
 % lmiguelgato@gmail.com
 
-nvar = 0.001;
+nvar = 0.001;   % measurement noise variance
 
-F = [1,   0, tau,   0; 
+% state transition model:
+F = [1,   0, tau,   0;  % 4x4
      0,   1,   0, tau;
      0,   0,   1,   0;
      0,   0,   0,   1];
- 
-G = [tau^2/2,       0;
+
+G = [tau^2/2,       0;  % 4x2
            0, tau^2/2;
          tau,       0;
             0,    tau];
-        
-Qw = sqrt([acc,   0;
+
+Qw = sqrt([acc,   0;    % 2x2
              0, acc]);
 
-R = [nvar,    0;
-        0, nvar];
-    
-Q = G*Qw*G';
- 
-H = [1, 0, 0, 0;
+Q = G*Qw*G';        % 4x4
+
+% measurement model:
+H = [1, 0, 0, 0;    % 2x4
      0, 1, 0, 0];
- 
+
+R = [nvar,    0;    % 2x2
+        0, nvar];
+
 %% prediction:
 x_ = F*x_;          % 4x1
 P_ = F*P*F' + Q;    % 4x4
@@ -34,7 +44,6 @@ P_ = F*P*F' + Q;    % 4x4
 %% update:
 K = (P_*H')/(H*P_*H' + R);  % 4x2
 x = x_ + K*(y - H*x_);      % 4x1
-P = (eye(4) - K*H)*P_;
+P = (eye(4) - K*H)*P_;      % 4x4
 
 end
-
