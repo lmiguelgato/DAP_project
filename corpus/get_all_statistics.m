3702fa6f6f3e1f4ea25f60d12f0c5960dc921d33
Luis M. Gato <luis.gato@copsonic-tech.com>
2020-02-17 18:21:02 -0600

    Measure false alarm rates, and detection rates for ODAS in the whole corpus

--- corpus/get_all_statistics.m
+++ corpus/get_all_statistics.m
@@ -11,6 +11,7 @@ subFolders = files(dirFlags);
 
 all_data = [];
 no_data  = [];
+all_missedSources = [];
 no_10 = 0;
 no_5 = 0;
 
@@ -50,6 +51,15 @@ for k = 1 : length(subFolders)
             '/', subFolders2(i).name, '/', subFolders3(h).name);
         
           cd ([main_path '/' thisPath])
+          
+          fileID = fopen('errors.txt','r');
+          C = textscan(fileID,'%s',1,'delimiter','\n', 'headerlines',1);
+          Pfa = sscanf(C{1}{1},'false alarm rate: %f');
+          frewind(fileID)
+          C = textscan(fileID,'%s',1,'delimiter','\n', 'headerlines',2);
+          missedSources = sscanf(C{1}{1},'missed sources: %u');
+          all_missedSources = [all_missedSources; [missedSources, nsources, Pfa]];
+          fclose(fileID);
           
           mae15 = 0;
           mae10 = 0;
@@ -131,12 +141,67 @@ end
 
 mm = size(all_data, 1);
 
-mean_mae15 = sum(all_data(:, 4))/mm;
-mean_mae10 = sum(all_data(:, 5))/(mm-no_10);
-mean_mae5 = sum(all_data(:, 6))/(mm-no_5);
-mean_rms15 = sum(all_data(:, 7))/mm;
-mean_rms10 = sum(all_data(:, 8))/(mm-no_10);
-mean_rms5 = sum(all_data(:, 9))/(mm-no_5);
+mean_mae15  = sum(all_data(:, 4))/mm;
+mean_mae10  = sum(all_data(:, 5))/(mm-no_10);
+mean_mae5   = sum(all_data(:, 6))/(mm-no_5);
+mean_rms15  = sum(all_data(:, 7))/mm;
+mean_rms10  = sum(all_data(:, 8))/(mm-no_10);
+mean_rms5   = sum(all_data(:, 9))/(mm-no_5);
 mean_e15    = mean(all_data(:, 10));
 mean_e10    = mean(all_data(:, 11));
 mean_e5     = mean(all_data(:, 12));
+
+Pmd = all_missedSources(:, 1)./all_missedSources(:, 2);
+
+N1 = sum(all_missedSources(:, 2) == 1);
+N01 = sum(all_missedSources(:, 1) == 0 & all_missedSources(:, 2) == 1);
+N11 = sum(all_missedSources(:, 1) == 1 & all_missedSources(:, 2) == 1);
+Pfa1 = mean(all_missedSources(all_missedSources(:, 2) == 1, 3));
+
+N2 = sum(all_missedSources(:, 2) == 2);
+N02 = sum(all_missedSources(:, 1) == 0 & all_missedSources(:, 2) == 2);
+N12 = sum(all_missedSources(:, 1) == 1 & all_missedSources(:, 2) == 2);
+N22 = sum(all_missedSources(:, 1) == 2 & all_missedSources(:, 2) == 2);
+Pfa2 = mean(all_missedSources(all_missedSources(:, 2) == 2, 3));
+
+N3 = sum(all_missedSources(:, 2) == 3);
+N03 = sum(all_missedSources(:, 1) == 0 & all_missedSources(:, 2) == 3);
+N13 = sum(all_missedSources(:, 1) == 1 & all_missedSources(:, 2) == 3);
+N23 = sum(all_missedSources(:, 1) == 2 & all_missedSources(:, 2) == 3);
+N33 = sum(all_missedSources(:, 1) == 3 & all_missedSources(:, 2) == 3);
+Pfa3 = mean(all_missedSources(all_missedSources(:, 2) == 3, 3));
+
+N4 = sum(all_missedSources(:, 2) == 4);
+N04 = sum(all_missedSources(:, 1) == 0 & all_missedSources(:, 2) == 4);
+N14 = sum(all_missedSources(:, 1) == 1 & all_missedSources(:, 2) == 4);
+N24 = sum(all_missedSources(:, 1) == 2 & all_missedSources(:, 2) == 4);
+N34 = sum(all_missedSources(:, 1) == 3 & all_missedSources(:, 2) == 4);
+N44 = sum(all_missedSources(:, 1) == 3 & all_missedSources(:, 2) == 4);
+Pfa4 = mean(all_missedSources(all_missedSources(:, 2) == 4, 3));
+
+disp('When 1 source:')
+disp(['* ' num2str(N11/N1*100) ' % of times no sources were detected'])
+disp(['* ' num2str(N01/N1*100) ' % of times one source was detected'])
+disp(['* ' num2str(Pfa1) ' % of detections were false alarms (outside +/- 15 degrees from any source)'])
+
+disp('When 2 sources:')
+disp(['* ' num2str(N22/N2*100) ' % of times no sources were detected'])
+disp(['* ' num2str(N12/N2*100) ' % of times one source was detected'])
+disp(['* ' num2str(N02/N2*100) ' % of times two sources were detected'])
+disp(['* ' num2str(Pfa2) ' % of detections were false alarms (outside +/- 15 degrees from any source)'])
+
+disp('When 3 sources:')
+disp(['* ' num2str(N33/N3*100) ' % of times no sources were detected'])
+disp(['* ' num2str(N23/N3*100) ' % of times one source was detected'])
+disp(['* ' num2str(N13/N3*100) ' % of times two sources were detected'])
+disp(['* ' num2str(N03/N3*100) ' % of times three sources were detected'])
+disp(['* ' num2str(Pfa3) ' % of detections were false alarms (outside +/- 15 degrees from any source)'])
+
+disp('When 4 sources:')
+disp(['* ' num2str(N44/N4*100) ' % of times no sources were detected'])
+disp(['* ' num2str(N34/N4*100) ' % of times one source was detected'])
+disp(['* ' num2str(N24/N4*100) ' % of times two sources were detected'])
+disp(['* ' num2str(N14/N4*100) ' % of times three sources were detected'])
+disp(['* ' num2str(N04/N4*100) ' % of times four sources were detected'])
+disp(['* ' num2str(Pfa4) ' % of detections were false alarms (outside +/- 15 degrees from any source)'])
+%N01 = sum(Pmd == 0.0 & all_missedSources(:, 2) == total_sources);
